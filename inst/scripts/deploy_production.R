32b6daad6eb8f96023c8876c1c144fbdaf3a69db
Eric Koncina <mail@koncina.eu>
2017-12-12 15:55:34 +0100

    enhance gitlab addin (fetch project id from API, better error handling)

--- inst/scripts/deploy_production.R
+++ inst/scripts/deploy_production.R
@@ -1,5 +1,5 @@
 local({
-  config <- purrr::map(rlang::set_names(c("gitlab_url", "gitlab_project_id", "gitlab_private_token", "gitlab_deploy_job")),
+  config <- purrr::map(rlang::set_names(c("gitlab_url", "gitlab_project_id", "gitlab_private_token", "gitlab_deploy_job", "gitlab_disable_confirm")),
                 rstudioapi::getPersistentValue)
   
   # We keep the project_id as a character and are not checking if it's a number: the request will fail
@@ -8,7 +8,7 @@ local({
     stop("Invalid gitlab configuration", call. = FALSE)
   }
   
-  if (!rstudioapi::showQuestion(title = "Confirm", ok = "OK", cancel = "Cancel", "Are you sure you want to deploy to production?")) return()
+  if (!isTRUE(as.logical(config$gitlab_disable_confirm)) && !rstudioapi::showQuestion(title = "Confirm", ok = "OK", cancel = "Cancel", "Are you sure you want to deploy to production?")) return()
   
-  gitlab_manual_job(config$gitlab_url, config$gitlab_project_id, config$gitlab_deploy_job, config$gitlab_private_token)
+  gitlab_manual_job(config$gitlab_url, config$gitlab_private_token, config$gitlab_project_id, config$gitlab_deploy_job)
 })
--- inst/scripts/gitlab_config.R
+++ inst/scripts/gitlab_config.R
@@ -4,25 +4,61 @@ local({
   }
   ui <- miniUI::miniPage(
     miniUI::gadgetTitleBar("Set gitlab settings"),
-    
     miniUI::miniContentPanel(
-      my_textInput("gitlab_url", "Gitlab url"),
-      my_textInput("gitlab_project_id", "Project ID (number)"),
-      my_textInput("gitlab_private_token", "Private token"),
-      my_textInput("gitlab_deploy_job", "Deploy job name")
+      shiny::fluidRow(
+        shiny::column(6,
+               shiny::wellPanel(
+                 my_textInput("gitlab_url", "Gitlab url"),
+                 my_textInput("gitlab_private_token", "Private token"),
+                 miniButtonBlock(
+                   actionButton("connect", "Connect to gitlab", primary = TRUE)
+                 )
+               )
+        ),
+        shiny::column(6,
+               shiny::wellPanel(
+                 shiny::selectInput("gitlab_project_id", label = "Select the project", 
+                             choices = rstudioapi::getPersistentValue("gitlab_project_id")),
+                 shiny::selectInput("gitlab_deploy_job", label = "Select the job", 
+                             choices = rstudioapi::getPersistentValue("gitlab_deploy_job")),
+                 shiny::checkboxInput("gitlab_disable_confirm", "Disable confirmation dialog", isTRUE(as.logical(rstudioapi::getPersistentValue("gitlab_disable_confirm"))))
+               )
+        )
+      )
     )
   )
   
-  server <- function(input, output) {
+  server <- function(input, output, session) {
+    
+    shiny::observeEvent(input$connect, {
+      projects <- purrr::safely(gitlab_projects)(input$gitlab_url, input$gitlab_private_token)
+      
+      if (!is_null(projects$error)) {
+        rstudioapi::showDialog("Error", projects[["error"]][["message"]], "")
+        return()
+      }
+      shiny::updateSelectInput(session, "gitlab_project_id",
+                        choices = projects$result)
+      
+    })
+    
+    shiny::observeEvent(input$gitlab_project_id, {
+      jobs <- gitlab_joblist(input$gitlab_url, input$gitlab_private_token, input$gitlab_project_id)[["name"]]
+      
+      shiny::updateSelectInput(session, "gitlab_deploy_job",
+                        choices = jobs)
+    }, ignoreInit = TRUE)
+    
     shiny::observeEvent(input$done, {
-      map(set_names(c("gitlab_url", "gitlab_project_id", "gitlab_private_token", "gitlab_deploy_job")), 
+      map(purrr::set_names(c("gitlab_url", "gitlab_project_id", "gitlab_private_token", "gitlab_deploy_job", "gitlab_disable_confirm")), 
           ~rstudioapi::setPersistentValue(.x, input[[.x]]))
       shiny::stopApp()
     })
+    
     shiny::observeEvent(input$cancel, {
       shiny::stopApp()
     })
+    
   }
-  
-  shiny::runGadget(ui, server, viewer = shiny::dialogViewer("bs2site gitlab settings", height = 400))
+  shiny::runGadget(ui, server, viewer = shiny::dialogViewer("bs2site gitlab settings", width = 800, height = 400))
 })
