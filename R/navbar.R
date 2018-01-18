5b097ceed6baf3c7dc37ed1218a03e892b42e162
Eric Koncina <mail@koncina.eu>
2018-01-18 11:14:39 +0100

    add option to keep title in the navbar

--- R/navbar.R
+++ R/navbar.R
@@ -11,18 +11,23 @@ NULL
 #'
 #' @param site_yml path to the `_site.yml` file.
 #' 
-#' @param logo_img path to the logo files
+#' @param logo_img vector containing the path to the logo file(s).
+#' 
+#' @param show_title boolean. If `TRUE`, the title will be shown next to the logo.
 #'
 #' @return NULL
 #'
 #' @export
-create_navbar <- function(site_yml = file.path("site", "site.yml"), logo_img) {
+create_navbar <- function(site_yml = file.path("site", "site.yml"), logo_img, show_title = FALSE) {
   yaml <- rmarkdown:::yaml_load_file_utf8(site_yml)
   
   input_code <- glue::glue("<a class=\"navbar-brand\" href=\"index.html\">{yaml[[\"navbar\"]][\"title\"]}</a>")
   
-  output_code <- map(logo_img, ~htmltools::a(htmltools::img(src = .), class = "navbar-brand", href = "index.html")) %>%
+  output_code <- map(logo_img, ~htmltools::img(src = .)) %>%
+    htmltools::span() %>%
+    htmltools::a(if_else(isTRUE(keep_text), yaml[["navbar"]]["title"], NULL), class = "navbar-brand", href = "index.html") %>%
     htmltools::renderTags()
+  
   output_code <- output_code[["html"]]
   navbar <- paste(readLines(rmarkdown::navbar_html(yaml[["navbar"]])), collapse = "\n")
   
