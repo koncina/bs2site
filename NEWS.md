e11726fa26e82f687e6150b2145dbaf662ad1e96
Eric Koncina <mail@koncina.eu>
2018-02-01 11:22:21 +0100

    update docs

--- NEWS.md
+++ NEWS.md
@@ -2,11 +2,9 @@
 
 - Added a `NEWS.md` file to track changes to the package.
 - Added `pkgdown` documentation
+- Added box colour definitions: intro, warning, practical... (requires `iosp` released after commit [11c9ab7](https://github.com/koncina/iosp/commit/11c9ab7bfa97dfe07c2fc9368de31d89379dbf4f)). Run `use_box_colours()` to setup the definitions.
 
 - Update the `create_navbar()` function to allow keeping the navbar title in addition to branding logos.
 - Navbar can be generated for pages in subfolders (new `relative_to` argument).
 - Fixed bug with empty list entries (unnecessary tibble coercion).
-
-
-
 
--- docs/articles/box-colours.R
+++ docs/articles/box-colours.R
@@ -11,21 +11,26 @@ cat(bs2site:::create_box_css(path = NULL))
 
 ## ---- echo = FALSE-------------------------------------------------------
 bs2site:::get_box_colours() %>%
+  select(name, description) %>%
   knitr::kable()
 
 ## ---- echo = FALSE-------------------------------------------------------
-draw_box <- function(colour) {
-  htmltools::h3("Header") %>% htmltools::div(class = glue::glue("box {colour}"),
+draw_box <- function(colour, outline = FALSE) {
+  htmltools::h3("Header") %>% htmltools::div(class = glue::glue("box {colour} {if_else(outline, 'outline', '')}"),
                                              "Some text using", htmltools::br(),
-                                             htmltools::code(colour)
+                                             htmltools::code(paste0(colour, if_else(outline, ' outline', '')))
                                              )
 }
 
 ## ---- echo = FALSE, results = "asis"-------------------------------------
 colours <- pull(bs2site:::get_box_colours(), name)
 
-glue::glue("bg-{colours}") %>%
-  purrr::map(draw_box) %>%
+purrr::map(colours, draw_box) %>%
+  htmltools::div(class = "box-container") %>%
+  htmltools::doRenderTags()
+
+## ---- echo = FALSE, results = "asis"-------------------------------------
+purrr::map(colours, draw_box, outline = TRUE) %>%
   htmltools::div(class = "box-container") %>%
   htmltools::doRenderTags()
 
