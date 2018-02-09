ad2b9054b778c0a21c3a28c66d29219623a7441b
Eric Koncina <mail@koncina.eu>
2018-02-09 09:14:24 +0100

    add clean build for active branch (replacing master only)

--- NEWS.md
+++ NEWS.md
@@ -3,6 +3,7 @@
 - Added a `NEWS.md` file to track changes to the package.
 - Added `pkgdown` documentation
 - Added box colour definitions: intro, warning, practical... (requires `iosp` released after commit [11c9ab7](https://github.com/koncina/iosp/commit/11c9ab7bfa97dfe07c2fc9368de31d89379dbf4f)). Run `use_box_colours()` to setup the definitions.
+- Added Rstudio addins to interact with the gitlab CI. You can trigger a clean build for the current branch or run the manual deploy job.
 
 - Update the `create_navbar()` function to allow keeping the navbar title in addition to branding logos.
 - Navbar can be generated for pages in subfolders (new `relative_to` argument).
--- inst/scripts/clean_build.R
+++ inst/scripts/clean_build.R
@@ -23,11 +23,20 @@ local({
     pipeline_token <- purrr::pluck(gitlab_create_trigger(config$gitlab_url, config$gitlab_private_token, config$gitlab_project_id), "token")
   }
   
+  # Try to detect the current branch
+  current_branch <- purrr::possibly(system, otherwise = NULL)("git rev-parse --abbrev-ref HEAD", inter = TRUE)
+  
+  if (is.null(current_branch)) {
+    current_branch <- "master"
+    warning("Could not detect current branch name: triggering a new build for `master`")
+  }
+  
   gitlab_curl(glue::glue("{config$gitlab_url}/api/v4/projects/{config$gitlab_project_id}/trigger/pipeline"),
               config$gitlab_private_token,
               status_code = 201,
               post = TRUE,
               forms = list(`variables[CLEAN]` = "true",
-                           ref = "master",
+                           ref = current_branch,
                            token = pipeline_token))
+
 })
